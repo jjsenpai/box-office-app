04ebe3068467d23238e5a076b95152cfab9f0a64
jjsenpai <ujjawalsharma354@gmail.com>
2023-07-14 02:08:16 +0530

    api handling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,31 @@
+import { useState } from 'react';
+
 const Home = () => {
-  return <div>Home Page</div>;
+  const [searchStr, setSearchStr] = useState('');
+
+  const onSearch = async ev => {
+    ev.preventDefault();
+    const info = (
+      await fetch(`https://api.tvmaze.com/search/shows?q=${searchStr}`)
+    ).json();
+    const body = await info;
+    console.log(body);
+  };
+
+  const onSearchInputChange = data => {
+    //for 2 way data transfer
+
+    setSearchStr(data.target.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSearch}>
+        <input type="text" value={searchStr} onChange={onSearchInputChange} />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default Home;
